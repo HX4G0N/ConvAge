5262908df3276b950923cdaef674ceb52c6ed7ed
Aleksandra Teska <aleksandra.teska@gmail.com>
2018-01-17 03:17:32 +0100

    vggface 2 layers with weights implemented inside segnet

--- vggface.py
+++ vggface.py
@@ -1,48 +1,31 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 from PIL import Image
+from solver import Solver
+from data_utils import FaceData
 from torch.utils.serialization import load_lua
-from torch.legacy.nn import SpatialConvolution
-from classifiers.conv_age import ConvAge
+from classifiers.conv_age import SegmentationNN
 
-'''
-#start: need to download .t7 file
-import torch.utils.model_zoo as model_zoo
-model_urls = {
-    'vgg_face': 'http://www.robots.ox.ac.uk/~vgg/software/vgg_face/src/vgg_face_torch.tar.gz'
-}
 
-def vgg_face():
-    model = model_zoo.load_url(model_urls['vgg_face'])
-    return model
-'''
+train_data = FaceData(image_paths_file='LAG/train/train.txt')
+val_data = FaceData(image_paths_file='LAG/val/val.txt')
+train_loader = torch.utils.data.DataLoader(train_data,
+                                           batch_size=1,
+                                           shuffle=True,
+                                           num_workers=1)
 
-image = Image.open('ak.png').convert('RGB')
-image = np.asarray(image)
-image = np.reshape(image,(3,224,224))
-image = torch.from_numpy(image).float()
+val_loader = torch.utils.data.DataLoader(val_data,
+                                         batch_size=1,
+                                         shuffle=False,
+                                         num_workers=1)
+#Model loading
+model = SegmentationNN()
 
-#Local implementation
-vgg_face_t7 = load_lua('classifiers/VGG_FACE.t7', unknown_classes=True)
-layer0 = SpatialConvolution(3 , 64, 3, 3, dW=1, dH=1, padW=1, padH=None)
-layer1 = SpatialConvolution(27, 64, 3, 3, dW=1, dH=1, padW=1, padH=None)
+solver = Solver(optim_args={"lr": 1e-4,
+                            "eps": 1e-8
+                            },
+                loss_func = torch.nn.CrossEntropyLoss(ignore_index = -1))
 
-#print('layer0 weights size:', layer0.weight.size())
-#print('loaded weights size:', vgg_face_t7.modules[0].weight.size())
-weights0 = vgg_face_t7.modules[0].weight
-weights1 = vgg_face_t7.modules[2].weight
-layer0.weight = weights0.resize_(3 , 64, 3, 3)
-layer1.weight = weights1.resize_(64, 64, 3, 3)
-
-print(weights0.size())
-print(weights1.size())
-
-out0 = layer0.forward(image)
-out1 = nn.ReLU(out0)
-#out2 = layer1.forward(out1) #size of input problem
-print(out0.size())
-
-#Loading from classifier
-model = ConvAge()
-out = model.forward(image)
+solver.train(model, train_loader, val_loader, log_nth=1, num_epochs=1)
