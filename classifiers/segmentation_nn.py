9a31cc4592981bf3f1efc28eefb33f9ebf989da0
Aleksandra Teska <aleksandra.teska@gmail.com>
2018-02-01 23:56:25 +0100

    vgg_face full weights loading in segmentation_nn

--- classifiers/segmentation_nn.py
+++ classifiers/segmentation_nn.py
@@ -2,13 +2,11 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.utils.serialization import load_lua
 
-
-
-class SegmentationNN(nn.Module):
-
-    def __init__(self, n_classes=256, in_channels=3, is_unpooling=True):
-        super(SegmentationNN, self).__init__()
+class ConvAgeNN(nn.Module):
+    def __init__(self, n_classes=256, in_channels=3, is_unpooling=True, init_weight=True):
+        super(ConvAgeNN, self).__init__()
 
         ########################################################################
         #                             YOUR CODE                                #
@@ -16,11 +14,17 @@ class SegmentationNN(nn.Module):
         self.in_channels = in_channels
         self.is_unpooling = is_unpooling
 
-        self.down1 = segnetDown2(self.in_channels, 64)
-        self.down2 = segnetDown2(64, 128)
-        self.down3 = segnetDown3(128, 256)
-        self.down4 = segnetDown3(256, 512)
-        self.down5 = segnetDown3(512, 512)
+        self.down1 = VGGDown2(self.in_channels, 64)
+        self.down2 = VGGDown2(64, 128)
+        self.down3 = VGGDown3(128, 256)
+        self.down4 = VGGDown3(256, 512)
+        self.down5 = VGGDown3(512, 512)
+
+        #self.down1 = segnetDown2(self.in_channels, 64)
+        #self.down2 = segnetDown2(64, 128)
+        #self.down3 = segnetDown3(128, 256)
+        #self.down4 = segnetDown3(256, 512)
+        #self.down5 = segnetDown3(512, 512)
 
         self.up5 = segnetUp3(512, 512)
         self.up4 = segnetUp3(512, 256)
@@ -28,6 +32,9 @@ class SegmentationNN(nn.Module):
         self.up2 = segnetUp2(128, 64)
         self.up1 = segnetUp2(64, n_classes)
         self.out = segnetOut(n_classes, 1)
+
+        if init_weight:
+            self.init_vggFACE_params()
 
     def forward(self, inputs):
 
@@ -46,44 +53,46 @@ class SegmentationNN(nn.Module):
 
         return out
 
+    def init_vggFACE_params(self):
+        vgg_face_t7 = load_lua('classifiers/VGG_FACE.t7', unknown_classes=True)
+        vgg_face_layers = [vgg_face_t7.modules[0].weight.resize_(64 , 3  , 3, 3),
+                           vgg_face_t7.modules[2].weight.resize_(64 , 64 , 3, 3),
+                           vgg_face_t7.modules[5].weight.resize_(128, 64 , 3, 3),
+                           vgg_face_t7.modules[7].weight.resize_(128, 128, 3, 3),
+                           vgg_face_t7.modules[10].weight.resize_(256, 128, 3, 3),
+                           vgg_face_t7.modules[12].weight.resize_(256, 256, 3, 3),
+                           vgg_face_t7.modules[14].weight.resize_(256, 256, 3, 3),
+                           vgg_face_t7.modules[17].weight.resize_(512, 256, 3, 3),
+                           vgg_face_t7.modules[19].weight.resize_(512, 512, 3, 3),
+                           vgg_face_t7.modules[21].weight.resize_(512, 512, 3, 3),
+                           vgg_face_t7.modules[24].weight.resize_(512, 512, 3, 3),
+                           vgg_face_t7.modules[26].weight.resize_(512, 512, 3, 3),
+                           vgg_face_t7.modules[28].weight.resize_(512, 512, 3, 3),]
 
-    def init_vgg16_params(self, vgg16):
         blocks = [self.down1,
                   self.down2,
                   self.down3,
                   self.down4,
-                  self.down5]
-
-        ranges = [[0, 4], [5, 9], [10, 16], [17, 23], [24, 29]]
-        features = list(vgg16.features.children())
-
-        vgg_layers = []
-        for _layer in features:
-            if isinstance(_layer, nn.Conv2d):
-                vgg_layers.append(_layer)
+                  self.down5,]
 
         merged_layers = []
         for idx, conv_block in enumerate(blocks):
             if idx < 2:
-                units = [conv_block.conv1.cbr_unit,
-                         conv_block.conv2.cbr_unit]
+                units = [conv_block.conv1.cb_unit,
+                         conv_block.conv2.cb_unit]
             else:
-                units = [conv_block.conv1.cbr_unit,
-                         conv_block.conv2.cbr_unit,
-                         conv_block.conv3.cbr_unit]
+                units = [conv_block.conv1.cb_unit,
+                         conv_block.conv2.cb_unit,
+                         conv_block.conv3.cb_unit]
             for _unit in units:
                 for _layer in _unit:
                     if isinstance(_layer, nn.Conv2d):
                         merged_layers.append(_layer)
 
-        assert len(vgg_layers) == len(merged_layers)
-
-        for l1, l2 in zip(vgg_layers, merged_layers):
-            if isinstance(l1, nn.Conv2d) and isinstance(l2, nn.Conv2d):
-                assert l1.weight.size() == l2.weight.size()
-                assert l1.bias.size() == l2.bias.size()
-                l2.weight.data = l1.weight.data
-                l2.bias.data = l1.bias.data
+        for l1, l2 in zip(vgg_face_layers, merged_layers):
+            if isinstance(l2, nn.Conv2d):
+                assert l1.size() == l2.weight.size()
+                l2.weight.data = l1
 
 
     @property
@@ -104,6 +113,47 @@ class SegmentationNN(nn.Module):
         print('Saving model... %s' % path)
         torch.save(self, path)
 
+## VGG_face layers:
+class conv2DRelu(nn.Module):
+    def __init__(self, in_channels, n_filters, k_size,  stride, padding, bias=True):
+        super(conv2DRelu, self).__init__()
+
+        self.cb_unit = nn.Sequential(nn.Conv2d(int(in_channels), int(n_filters), kernel_size=k_size,
+                                               padding=padding, stride=stride, bias=bias),
+                                               nn.ReLU(inplace=True),)
+    def forward(self, inputs):
+        outputs = self.cb_unit(inputs)
+        return outputs
+
+class VGGDown2(nn.Module):
+    def __init__(self, in_size, out_size):
+        super(VGGDown2, self).__init__()
+        self.conv1 = conv2DRelu(in_size, out_size, 3, 1, 1)
+        self.conv2 = conv2DRelu(out_size, out_size, 3, 1, 1)
+        self.maxpool_with_argmax = nn.MaxPool2d(2, 2, return_indices=True)
+
+    def forward(self, inputs):
+        outputs = self.conv1(inputs)
+        outputs = self.conv2(outputs)
+        unpooled_shape = outputs.size()
+        outputs, indices = self.maxpool_with_argmax(outputs)
+        return outputs, indices, unpooled_shape
+
+class VGGDown3(nn.Module):
+    def __init__(self, in_size, out_size):
+        super(VGGDown3, self).__init__()
+        self.conv1 = conv2DRelu(in_size, out_size, 3, 1, 1)
+        self.conv2 = conv2DRelu(out_size, out_size, 3, 1, 1)
+        self.conv3 = conv2DRelu(out_size, out_size, 3, 1, 1)
+        self.maxpool_with_argmax = nn.MaxPool2d(2, 2, return_indices=True)
+
+    def forward(self, inputs):
+        outputs = self.conv1(inputs)
+        outputs = self.conv2(outputs)
+        outputs = self.conv3(outputs)
+        unpooled_shape = outputs.size()
+        outputs, indices = self.maxpool_with_argmax(outputs)
+        return outputs, indices, unpooled_shape
 
 class segnetDown2(nn.Module):
     def __init__(self, in_size, out_size):
@@ -219,7 +269,7 @@ class deconv2DBatchNormRelu(nn.Module):
     def forward(self, inputs):
         outputs = self.dcbr_unit(inputs)
         return outputs
-    
+
 class segnetOut(nn.Module):
     def __init__(self, in_size, out_size):
         super(segnetOut, self).__init__()
